3424c6ef4cce30066dd1717395c973a203303e5e
hpuma <pumahenry@live.com>
2021-03-01 02:06:24 -0500

    Set up tab bar 3 --> 4

--- Flixster/MovieDetailsViewController.swift
+++ Flixster/MovieDetailsViewController.swift
@@ -36,10 +36,7 @@ class MovieDetailsViewController: UIViewController {
         let backdropPath = movie["backdrop_path"] as! String
         let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
         
-
         backdropView.af_setImage(withURL: backdropUrl!)
-        
-        
         
         
     }
---
+++ Flixster/MovieGridViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieGridViewController.swift
+//  Flixster
+//
+//  Created by hpuma on 3/1/21.
+//
+
+import UIKit
+
+class MovieGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
