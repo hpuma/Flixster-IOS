d1fcf7ceb93772a8e9b70d75fadfb361efdd4339
hpuma <pumahenry@live.com>
2021-03-01 01:45:23 -0500

    Save from movie details 2 --> 3

--- Flixster/MovieDetailsViewController.swift
+++ Flixster/MovieDetailsViewController.swift
@@ -6,15 +6,42 @@
 //
 
 import UIKit
-
+import AlamofireImage
 class MovieDetailsViewController: UIViewController {
     var movie: [String:Any]!
     
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print(movie["title"])
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["overview"] as? String
+        // So the synopsis text does not clip
+        synopsisLabel.sizeToFit()
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        // This url enforces url text formatting, make sure you have https:// etc ...
+        let posterUrl = URL(string: baseUrl + posterPath)
+        // Using from pod AlamofireImage, this handles downloading the image from the url and then displaying
+        posterView.af_setImage(withURL: posterUrl!)
+        
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+
+        backdropView.af_setImage(withURL: backdropUrl!)
+        
+        
+        
+        
     }
     
 
