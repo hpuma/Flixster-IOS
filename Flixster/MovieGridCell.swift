7f66426028ed71de3d9aa34595591cff544be812
hpuma <pumahenry@live.com>
2021-03-01 02:55:31 -0500

    Finished with movie grid

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by hpuma on 3/1/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
